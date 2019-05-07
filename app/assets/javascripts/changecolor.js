f4a8c53487e5319052d6141fa8b4531798da68b8
Sebastian Poblete <sebapobletec@gmail.com>
2019-05-06 21:58:04 -0400

    last commit

--- app/assets/javascripts/changecolor.js
+++ app/assets/javascripts/changecolor.js
@@ -6,11 +6,20 @@
               var R = $("#Red").val()
               var G = $("#Green").val()
               var B = $("#Blue").val()
-              var str = "("+R +","+ G + "," + B+")";
-              $("#color").text(str)
+              var str = "("+R+","+G+","+B+")";
+
+              $("#redtext").text(R)
+              $("#redtext").css("color","rgb("+R+",0,0")
+
+              $("#greentext").text("|"+G)
+              $("#greentext").css("color","rgb(0,"+ G +",0")
+
+              $("#bluetext").text("|"+B+")")
+              $("#bluetext").css("color","rgb(0,0,"+B)
+
               $("#box").css("background-color","rgb"+str);
 
-                        
+
 
               }
         });
